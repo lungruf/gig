1a989db3afe0cffaf26c0b087bac66a054d87706
Asped <drukon2012@ya.ru>
2023-10-02 04:02:42 +0300

    ДЗ

--- dz.py
+++ dz.py
@@ -347,9 +347,11 @@ def dat ():
         fail.write(f'{name} {surname}\n{tree_name}\n{telefon}\n\n')
         
 def prin_contact():
-    with open('contacts.txt''r') as dart:
+    with open('contacts.txt','r') as dart:
      asur = dart.read()
      print(asur)
+    menu()
+    playmenu()
 
 def poisc ():
     serch = input("Ведите имя ").title()
@@ -360,6 +362,8 @@ def poisc ():
                 print("")
                 print (i)
                 print("")
+    menu()
+    playmenu()
         
 def delit ():
     print("Выбирите индекс контакта который хотите удалить")
@@ -375,4 +379,30 @@ def delit ():
          for v in alert:
              for x in v:
                  print(x)
-delit ()
+    menu()
+    playmenu()
+    
+
+
+def menu(): 
+    print("  Menu  ")
+    print("_____________________________")
+    print(" (1) -- Показать контакты ")
+    print(" (2) -- Добавить контакт")
+    print(" (3) -- Найти контакт")
+    print(" (4) -- Удалить контакт")
+    print(" (5) --  Exit")
+    print("_____________________________")
+
+menu()
+def playmenu():
+ play = int(input(""))
+ if play == 1:
+    prin_contact()
+ elif play == 2:
+    dat()
+ elif play == 3:
+     poisc()
+ elif play == 4:
+     delit()
+playmenu()

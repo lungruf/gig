443c03ff754492b31e48ae78e593dfaa93553062
Asped <drukon2012@ya.ru>
2023-10-09 12:18:54 +0300

    ДЗ

--- dzz.py
+++ dzz.py
@@ -1,3 +1,7 @@
+"""
+В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
+Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+"""
 import pandas as pd 
 import numpy as np 
 import random
